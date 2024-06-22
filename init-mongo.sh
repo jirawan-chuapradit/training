920e5d60dbeb73b73b40e5a9a1f246be0df690cb
Jirawan.C <jirawan-chuapradit@users.noreply.github.com>
2024-06-22 10:17:48 +0700

    add db

--- init-mongo.sh
+++ init-mongo.sh
@@ -1,13 +1,14 @@
-mongo <<EOF
-use $MONGO_INITDB_DATABASE
-var rootUser = '$MONGO_INITDB_ROOT_USERNAME'
-var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD'
-var admin = db.getSiblingDB('admin')
-admin.auth(rootUser, rootPassword)
-
-db.createUser({
-  user: user,
-  pwd: passwd,
-  roles: ["readWrite"]
-})
+mongosh -- "$MONGO_INITDB_DATABASE" <<EOF
+    var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
+    var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
+    var admin = db.getSiblingDB('admin');
+    admin.auth(rootUser, rootPassword);
+    var user = '$MONGO_INITDB_USERNAME';
+    var passwd = '$MONGO_INITDB_PASSWORD';
+ 
+    db.createUser({
+      user: user,
+      pwd: passwd,
+      roles: ["readWrite"]
+    });
 EOF
